cf28b81bb2ab3e1b9976d9f1619aad7f431428f3
bruce <brucewang997@gmail.com>
2021-10-23 14:23:50 +0800

    fix

--- contracts/GameStarCustody.sol
+++ contracts/GameStarCustody.sol
@@ -1,11 +1,10 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "./includes/Ownable.sol";
+import "./includes/IERC20.sol";
+import "./includes/SafeMath.sol";
+import "./includes/SafeERC20.sol";
+import "./includes/ReentrancyGuard.sol";
 
 //
 // gameStarCustody contract
